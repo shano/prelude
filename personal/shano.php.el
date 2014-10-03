aad2d7ea32d90f857baaedad044003563930bc73
Shane Dowling <shane@shanedowling.com>
2014-10-03 12:25:54 +0100

    Adding flymake and phpcs

--- personal/shano.php.el
+++ personal/shano.php.el
@@ -3,5 +3,15 @@
 (global-set-key (kbd "C-c b") 'codebug)
 
 
+;; Require flymake php packages
+(prelude-require-package 'flymake-php)
+(prelude-require-package 'flymake-phpcs)
+
+(add-hook 'php-mode-hook 'flymake-php-load)
+(add-hook 'php-mode-hook 'flymake-phpcs-load)
+(custom-set-variables
+   '(flymake-phpcs-standard "PSR2"))
+
+
 (provide 'shano.php)
 ;;; shano.php ends here
