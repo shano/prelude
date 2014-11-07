ab2ae5a154551a686d125f0e89f44115b03ff85d
Shane Dowling <shane@shanedowling.com>
2014-11-07 13:23:06 +0000

    Adding hash key fix

--- personal/shano.tweaks.el
+++ personal/shano.tweaks.el
@@ -5,5 +5,8 @@
 (prelude-require-package 'ag)
 (global-set-key (kbd "C-h s") 'ag)
 
+;; Allow hash to be entered
+(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
+
 (provide 'shano.tweaks)
 ;;; shano.tweaks ends here
